ca95463a608c4bd055a18cfe414dc976ce613aab
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-06 19:17:32 +0600

    Add implementation for E. Interval Sweep problem; update D. Ali Baba and Puzzles problem with additional test case

--- D_Ali_Baba_and_Puzzles.cpp
+++ D_Ali_Baba_and_Puzzles.cpp
@@ -1,23 +1,23 @@
-#include<bits/stdc++.h>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
+    long long a, b, c, d;
+    cin >> a >> b >> c >> d;
 
-    long long a,b,c,d;
-    cin >> a>>b>>c>>d;
-
-    if (a + b + c == d || a + b - c == d || a + b * c == d || a - b + c == d ||
-        a - b - c == d || a - b * c == d || a * b + c == d || a * b - c == d ||
-        a * b * c == d)
+    if (a + b - c == d ||
+        a + b * c == d ||
+        a - b + c == d ||
+        a - b * c == d ||
+        a * b + c == d ||
+        a * b - c == d)
     {
-        cout<<"YES\n";
+        cout << "YES\n";
     }
-    else{
-        cout<<"NO\n";
+    else
+    {
+        cout << "NO\n";
     }
-    
-    
-    
+
     return 0;
 }
