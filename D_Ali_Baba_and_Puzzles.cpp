7cb81eb13aa5c374d6832f5a491fa62d1d61d102
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-08-06 19:00:13 +0600

    Add initial implementation for D. Ali Baba and Puzzles problem

---
+++ D_Ali_Baba_and_Puzzles.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    long long a,b,c,d;
+    cin >> a>>b>>c>>d;
+
+    if (a + b + c == d || a + b - c == d || a + b * c == d || a - b + c == d ||
+        a - b - c == d || a - b * c == d || a * b + c == d || a * b - c == d ||
+        a * b * c == d)
+    {
+        cout<<"YES\n";
+    }
+    else{
+        cout<<"NO\n";
+    }
+    
+    
+    
+    return 0;
+}
